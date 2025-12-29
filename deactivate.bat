74bae4fa95b57273f08788a850da1881d69a38de
liubomyr-lototskyi <lototskyi.liubomyr@gmail.com>
2025-12-29 16:48:23 +0200

    Update deactivate.bat

--- deactivate.bat
+++ deactivate.bat
@@ -22,10 +22,7 @@ set "ROPE_NEXT_ROOT="
 set "EXT_DEPENDENCIES="
 set "FFMPEG_PATH="
 
-new_hash = self._calculate_hash(filepath)
-file_stat = os.stat(filepath)
-self.database[filepath]["hash"] = new_hash
-self.database[filepath]["size"] = file_stat.st_size
+
 
 REM Gestione della variabile PATH
 if command == "add":
@@ -38,4 +35,5 @@ if command == "add":
         print_usage()
         sys.exit(1)
 set "_OLD_PATH="
+
 
